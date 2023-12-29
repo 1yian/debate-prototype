b8821087c0ccfc03ca34428b4520bedfb47c5a40
1yian <yian@utexas.edu>
2023-12-28 22:40:33 -0600

    Refactoring and reworking

--- app.py
+++ app.py
@@ -1,148 +1,154 @@
-import openai
-import json
-from summarizer.sbert import SBertSummarizer
-import os
-
-import google.generativeai as genai
-OPENAI_KEY = os.environ.get('OPENAI_KEY')
-GOOGLE_KEY = os.environ.get('GOOGLE_KEY')
-genai.configure(api_key=GOOGLE_KEY)
-PERSONA_GEN_PROMPT = (
-        f"Given the topic [TOPIC], create a roundtable debate of different personas "
-        f"to show key perspectives on the issue. Output the personas as a list "
-        f"of JSON objects. Each JSON object should have the following structure: "
-        f"{{'name': 'Name of the Persona',"
-        f"'description': 'Brief Description of the Persona'}}. Ensure that the output is "
-        f"formatted as valid JSON. Please generate exactly [NUM_PERSONAS] personas."
-        )
-START_DEBATE_PROMPT = (f"You are in a roundtable debate on the topic [TOPIC]. "
-          f"You are [NAME], who is [DESC]. "
-          f"Please start the debate by concisely presenting your argument for your stance on the topic.")
-DEBATE_PROMPT = (f"You are in a continuing roundtable debate on the topic [TOPIC]. "
-          f"You are [NAME], who is [DESC]. "
-          f"Here is the transcript of the debate so far: [HISTORY] "
-          f"Please continue to debate the others, concisely supporting your stance on the topic.")
-SHORTEN_AFTER = 2500
-TEMPERATURE = 0.7
-# Function to call OpenAI's API
-def call_openai(prompt, temperature=0.7):
-    openai.api_key = OPENAI_KEY
-    pa_prompt = [
-        {"role": "user",
-         "content": prompt}
-    ]
-    response = openai.ChatCompletion.create(
-        model=MODEL_NAME,
-        messages=pa_prompt,
-        temperature=temperature,
-    )
-    return response.choices[0].message['content'].strip()
-
-def call_google(prompt, temperature=0.7):
-    model = genai.GenerativeModel('gemini-pro',
-        generation_config=genai.types.GenerationConfig(
-        temperature=temperature)
-    )
-    return model.generate_content(prompt).text
-
-# Step 1: User Input for Debate Topic
-def get_debate_topic():
-    return input("Enter a debate topic: ")
-
-# Step 2: Persona Generation
-def extract_json_from_response(response):
-    try:
-        # Find the start and end of the JSON object/array
-        start = response.find('[')
-        end = response.rfind(']') + 1  # +1 to include the closing brace
-        if start != -1 and end != -1:
-            json_str = response[start:end]
-            return json.loads(json_str)
-        else:
-            raise ValueError("JSON structure not found in response.")
-    except json.JSONDecodeError as e:
-        raise ValueError(f"Failed to parse JSON: {e}")
-
-
-def generate_personas(topic):
-    prompt = PERSONA_GEN_PROMPT
-    prompt = prompt.replace("[TOPIC]", topic).replace("[NUM_PERSONAS]", str(int(NUM_PERSONAS)))
-
-    response = get_response(prompt, TEMPERATURE)
-    return extract_json_from_response(response)
-
-
-
-# Step 3: Debate Simulation
-model = SBertSummarizer('paraphrase-MiniLM-L6-v2')
-def check_shorten(debate_history):
-    num_words = len(str(debate_history).split())
-    if num_words > SHORTEN_AFTER:
-        print("SHORTENING...")
-        ratio = (SHORTEN_AFTER - 100) / num_words
-        new_debate_history = []
-        for line in debate_history:
-            line = line.copy()
-            new_debate_history.append(model(line['argument'], ratio))
-    else:
-        print("NOT SHORTENING...", num_words)
-        new_debate_history = debate_history
-    return new_debate_history
-
 import streamlit as st
-st.set_page_config(layout="wide")
-from summarizer.sbert import SBertSummarizer
-status_message = st.sidebar.empty()
-model = SBertSummarizer('paraphrase-MiniLM-L6-v2')
+import yaml
 
-def simulate_debate(topic, personas, rounds=3):
-    debate_history = []
-    print("here")
-    for i in range(int(rounds)):
-        for persona in personas:
-            status_message.text(f"On debate round {i + 1}, generating for {persona['name']}")
-            prompt = START_DEBATE_PROMPT if len(debate_history) == 0 else DEBATE_PROMPT
-            str_debate = str(check_shorten(debate_history))
-            prompt = prompt.replace("[TOPIC]", topic).replace("[NAME]", persona['name']).replace("[DESC]", persona['description']).replace("[HISTORY]", str_debate)
-            response = get_response(prompt, TEMPERATURE)
+from llm import call_llm, SUPPORTED_MODELS
+import llm
+DEFAULT_CFG_PATH = './default.yaml'
 
-            # Streamlit chat message
-            with st.chat_message(persona['name']):
-                st.write(f"{persona['name']}:\n", response)
 
-            debate_history.append({"persona": persona['name'], "argument": response})
+def draw_sidebar(cfg):
+    cfg['ux_params']['show_persona_desc'] = \
+        st.sidebar.toggle(
+            "Show personas",
+            cfg['ux_params']['show_persona_desc']
+        )
 
-def display_personas(personas):
-    st.subheader("Generated Personas")
-    for persona in personas:
-        with st.expander(persona['name']):
-            st.write(persona['description'])
+    with st.sidebar.expander('Prompts:'):
 
-def main():
-    st.title("Roundtable Debate Simulation")
+        cfg['prompts']['persona_creation'] = \
+            st.text_area(
+                'Persona Generation Prompt\n'
+                '(Use [TOPIC], [NUM_PERSONAS] in the prompt)',
+                cfg['prompts']['persona_creation'],
+            )
 
-    # Allow users to modify parameters
-    global PERSONA_GEN_PROMPT, START_DEBATE_PROMPT, DEBATE_PROMPT, SHORTEN_AFTER, NUM_ROUNDS, TEMPERATURE, NUM_PERSONAS, MODEL_NAME
-    PERSONA_GEN_PROMPT = st.sidebar.text_area("Persona Generation Prompt \n(Use [TOPIC], [NUM_PERSONAS] in your prompt)", PERSONA_GEN_PROMPT)
-    START_DEBATE_PROMPT = st.sidebar.text_area("Start Debate Prompt \n(Use [TOPIC], [NAME], [DESC] in your prompt)", START_DEBATE_PROMPT)
-    DEBATE_PROMPT = st.sidebar.text_area("Debate Prompt \n(Use [TOPIC], [NAME], [DESC], [HISTORY] in your prompt)", DEBATE_PROMPT)
-    SHORTEN_AFTER = st.sidebar.number_input("Shorten after (Max word length of debate transcript before we summarize)", value=SHORTEN_AFTER, step=100)
-    NUM_PERSONAS = st.sidebar.number_input("Number of personas", value=4, step=1)
-    NUM_ROUNDS = st.sidebar.number_input("Number of debate rounds", value=2, step=1)
-    TEMPERATURE = st.sidebar.number_input("Temperature (for GPT)", value=0.7, step=0.1)
-    MODEL_NAME = st.sidebar.selectbox("Model:", ['gemini-pro', 'gpt-3.5-turbo', 'gpt-4'])
+        cfg['prompts']['debate_start'] = \
+            st.text_area(
+                'Starting Round Debate Prompt\n'
+                '(Use [TOPIC], [NAME], [DESC] in the prompt)',
+                cfg['prompts']['debate_start'],
+            )
 
-    global get_response
-    get_response = call_openai if 'gpt' in MODEL_NAME else call_google
-    status_message.text("Waiting for topic...")
-    topic = st.text_input("Enter a debate topic: ")
+        cfg['prompts']['debate'] = \
+            st.text_area(
+                'Regular Debate Prompt\n'
+                '(Use [TOPIC], [NAME], [DESC], [HISTORY] in the prompt)',
+                cfg['prompts']['debate'],
+            )
+    with st.sidebar.expander('Debate Settings', expanded=True):
+        cfg['debate_params']['enable_continuous_mode'] = \
+            st.toggle(
+                'Continuous Mode',
+                cfg['debate_params']['enable_continuous_mode']
+            )
+
+        cfg['debate_params']['generate_personas'] = \
+            st.toggle(
+                'Generate personas', cfg['debate_params']['generate_personas']
+            )
+
+        cfg['debate_params']['transcript_word_limit'] = \
+            st.number_input(
+                'Transcript summarization threshold (words)',
+                value=cfg['debate_params']['transcript_word_limit'],
+                step=100,
+                min_value=100,
+                max_value=512_000,
+            )
+        cfg['debate_params']['num_personas'] = \
+            st.number_input(
+                'Number of personas to generate',
+                value=cfg['debate_params']['num_personas'],
+                step=1,
+                min_value=2,
+                max_value=150,
+            )
+        cfg['debate_params']['num_debate_rounds'] = \
+            st.number_input(
+                'Number of debate_rounds',
+                value=cfg['debate_params']['num_debate_rounds'],
+                step=1,
+                min_value=1,
+                max_value=50
+            )
+
+    with st.sidebar.expander('LLM Params (limited for now):'):
+
+        cfg['llm_params']['model_name'] = \
+            st.selectbox('Model', SUPPORTED_MODELS)
+
+        cfg['llm_params']['temperature'] = \
+            st.number_input(
+                'Temperature',
+                value=cfg['llm_params']['temperature'],
+                step=0.1,
+                min_value=0.0,
+                max_value=2.0,
+            )
+
+def draw_personas(personas, persona_tab):
+    for i, persona in enumerate(personas):
+        persona['name'] = st.text_input(
+            f"Persona {i} Name",
+            value=persona['name'],
+            placeholder="Enter persona name here."
+                        " For example, 'Dr. John Smith' or 'Doctor'",
+            label_visibility='collapsed'
+        )
+
+        persona['description'] = st.text_area(
+            f"Persona {i} Description",
+            value=persona['description'],
+            placeholder="Enter persona description here."
+                        " For example, 'Believes that vaccines cause autism.'",
+            label_visibility='collapsed'
+        )
+
+if __name__ == '__main__':
+    # Load the default config from default.yaml
+    with open(DEFAULT_CFG_PATH, 'r') as tmp_fd:
+        cfg = yaml.safe_load(tmp_fd)
+
+    # Define the initial layout of the website
+    st.title('Debate Simulation')
+    reset = st.button('Reset')
+    status_message = st.sidebar.empty()
+    draw_sidebar(cfg)
+    status_message.text('Waiting for topic...')
+
+    # Reset the experiment
+    if reset:
+        for key in st.session_state.keys():
+            del st.session_state[key]
+
+    # Get a topic from the user.
+    topic = st.text_input("Topic", placeholder='Enter a debate topic: ')
+
+    top_container = st.container()
+
+    # Define two tabs- personas and debate
+    # TODO Remove the tab if we aren't showing personas
+    persona_tab, debate_tab = st.tabs(["Personas", "Debate"])
+
 
     if topic:
-        status_message.text("Generating personas...")
-        personas = generate_personas(topic)
-        display_personas(personas)
-        status_message.text("Starting debate...")
-        simulate_debate(topic, personas, NUM_ROUNDS)
+        with top_container:
+            if not 'personas' in st.session_state:
+                with st.spinner('Generating personas...'):
+                    personas = llm.generate_personas(topic, cfg)
+                    st.session_state['personas'] = personas
 
-if __name__ == "__main__":
-    main()
+
+            if cfg['ux_params']['show_persona_desc'] and 'personas' in st.session_state:
+                with persona_tab:
+                    draw_personas(st.session_state.personas, persona_tab)
+            start = st.button('Start debate...')
+            stop = st.button('Stop debate...')
+
+            if stop:
+                st.stop()
+
+            if start and 'personas' in st.session_state:
+                with st.spinner('Debate in progress...'):
+                    with debate_tab:
+                        llm.simulate_debate(topic, st.session_state.personas, cfg)
